c65988ef687cd8a84c00a389431433b53951be4d
BBaoVanC <github@bbaovanc.com>
2021-03-17 20:31:47 -0500

    Add config
    Mostly from https://github.com/BBaoVanC/dwmblocks_rewrite

---
+++ blocks.h
@@ -0,0 +1,12 @@
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{ " ",		"cpu.block",		2,			8 },
+	{ " ",		"memory.block",		30,			9 },
+	{ " ",		"keymap.block",		0,			2 },
+	{ "",		"volume.block",		0,			1 }, /* no icon because it's calculated in volume.block */
+	{ "",		"battery.block",	60,			6 }, /* ^^^ same here but battery.block */
+	{ " ",		"date.block",		1,			7 },
+};
+
+static char delim[] = " | "; /* NULL character means no delimeter */
+static unsigned int delimLen = 5;
