8fd19b7f30945236bf67cc47aa838cf38afc94a0
BBaoVanC <github@bbaovanc.com>
2021-03-18 19:54:55 -0500

    Change update interval of memory block from 30s to 2s

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{ " ",		"cpu.block",		2,			8 },
-	{ " ",		"memory.block",		30,			9 },
+	{ " ",		"memory.block",		2,			9 },
 	{ " ",		"keymap.block",		0,			2 },
 	{ "",		"volume.block",		0,			1 }, /* no icon because it's calculated in volume.block */
 	{ "",		"battery.block",	60,			6 }, /* ^^^ same here but battery.block */
